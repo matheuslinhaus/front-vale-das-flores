23c33cd3c0a313c978938c2ef4845c67b95503c6
Matheus R Linhaus <matheuslinhauss@hotmail.com>
2025-01-08 17:52:35 -0300

    Criado pagina para cadastro de novos usuarios e login

---
+++ src/views/ContactPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Bem-vindo à nossa página inicial!</p>
+  </div>
+</template>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container">
+        <h2>Login</h2>
+        <form @submit.prevent="login">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" v-model="email" placeholder="Digite seu email" required
+                    aria-describedby="email-helper" />
+            </div>
+
+            <div class="form-group">
+                <label for="password">Senha</label>
+                <input type="password" id="password" v-model="password" placeholder="Digite sua senha" required
+                    aria-describedby="password-helper" />
+            </div>
+
+            <button type="submit" :disabled="!email || !password">Entrar</button>
+        </form>
+
+        <div class="signup-link">
+            <p>Ainda não tem uma conta? <router-link to="/register">Crie uma conta</router-link></p>
+        </div>
+
+        <div v-if="popupMessage">
+            <div class="popup-backdrop" @click="closePopup"></div>
+            <div class="popup">
+                <p>{{ popupMessage }}</p>
+                <button @click="closePopup">Fechar</button>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import "../css/Form.css";
+import "../css/Popup.css";
+import axios from "axios";
+
+export default {
+    name: "Login",
+    data() {
+        return {
+            email: "",
+            password: "",
+            popupMessage: null,
+        };
+    },
+    methods: {
+        async login() {
+            try {
+                const response = await axios.post("http://localhost:8080/api/auth/login", {
+                    email: this.email,
+                    password: this.password
+                });
+                localStorage.setItem("authToken", response.data.token);
+                this.$router.push("/users");
+            } catch (error) {
+                console.error('Erro no login:', error.response ? error.response.data : error);
+                this.popupMessage = 'Email ou senha incorretos.';
+            }
+        },
+        closePopup() {
+            this.popupMessage = null;
+        }
+    }
+};
+</script>
